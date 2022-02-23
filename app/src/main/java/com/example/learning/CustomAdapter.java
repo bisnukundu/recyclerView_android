920ea4e987359dc1167593462347655de7210029
Bisnu kundu <bisnukundu@gmail.com>
2022-02-23 22:01:57 +0600

    add insert method and update method and delete method

--- app/src/main/java/com/example/learning/CustomAdapter.java
+++ app/src/main/java/com/example/learning/CustomAdapter.java
@@ -1,12 +1,21 @@
 package com.example.learning;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,8 +23,9 @@ import java.util.ArrayList;
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.myViewHolder> {
     ArrayList<Data> contacts;
     Context context;
+    private int position;
 
-    public CustomAdapter(Context context, ArrayList contacts){
+    public CustomAdapter(Context context, ArrayList contacts) {
         this.contacts = contacts;
         this.context = context;
     }
@@ -23,7 +33,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.myViewHold
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.number,parent,false);
+        View v = LayoutInflater.from(context).inflate(R.layout.number, parent, false);
         myViewHolder vh = new myViewHolder(v);
         return vh;
     }
@@ -35,16 +45,83 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.myViewHold
 
     @Override
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
-            holder.name.setText(contacts.get(position).name);
-            holder.number.setText(contacts.get(position).number);
+        int p = position;
+        holder.name.setText(contacts.get(position).name+position);
+        holder.number.setText(contacts.get(position).number);
+
+//LongPress action
+        holder.row.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                action(p);
+                return true;
+            }
+        });
+
+//Set Animation
+        Animation myAnimation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+        holder.row.startAnimation(myAnimation);
     }
 
-    public class myViewHolder extends RecyclerView.ViewHolder{
-        TextView name,number;
+    public class myViewHolder extends RecyclerView.ViewHolder {
+        TextView name, number;
+        CardView row;
+
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.name);
             number = itemView.findViewById(R.id.number);
+            row = itemView.findViewById(R.id.row);
+
         }
+    }
+
+    ////////////////////////-----Custom Method Created by Bisnu kundu-------/////////////////////
+    //LongPress Dialog show
+    public void action(int position) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context)
+                .setTitle("Action id " +position)
+                .setMessage("What do you want to do?")
+                .setIcon(R.drawable.ic_launcher_foreground)
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteItem(position);
+                    }
+                }).setNegativeButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        updateItem(position);
+                    }
+                });
+        alert.show();
+    }
+
+    //Update Item
+    public void updateItem(int position) {
+        Dialog updateDialog = new Dialog(context);
+        updateDialog.setContentView(R.layout.dialog);
+        updateDialog.show();
+
+        Button insert_btn = updateDialog.findViewById(R.id.insert_btn);
+        EditText name = updateDialog.findViewById(R.id.edt_name);
+        EditText number = updateDialog.findViewById(R.id.edt_number);
+        insert_btn.setText("Update");
+        insert_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contacts.set(position, new Data(name.getText().toString(), number.getText().toString()));
+                notifyItemChanged(position);
+    updateDialog.dismiss();
+                Toast.makeText(context, "Update", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void deleteItem(int position) {
+        contacts.remove(position);
+        notifyItemRemoved(position);
+        Toast.makeText(context, "Delete Successfully " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/learning/MainActivity.java
+++ app/src/main/java/com/example/learning/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -19,31 +22,29 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 RecyclerView myRecycle;
     ArrayList<Data> data = new ArrayList<>();
+    CustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        data.add(new Data("Bisnu","017252525"));
-        data.add(new Data("Badhon","0172523625"));
-        data.add(new Data("Pram","01725236525"));
-        data.add(new Data("Noman","01725222525"));
-        data.add(new Data("Soyob","017252745"));
-        data.add(new Data("Bisnu","017252525"));
-        data.add(new Data("Badhon","0172523625"));
-        data.add(new Data("Pram","01725236525"));
-        data.add(new Data("Noman","01725222525"));
-        data.add(new Data("Soyob","017252745"));
-        data.add(new Data("Bisnu","017252525"));
-        data.add(new Data("Badhon","0172523625"));
-        data.add(new Data("Pram","01725236525"));
-        data.add(new Data("Noman","01725222525"));
+        data.add(new Data("A","017252525"));
+        data.add(new Data("B","0172523625"));
+        data.add(new Data("C","01725236525"));
+        data.add(new Data("D","01725222525"));
+        data.add(new Data("E","017252745"));
+        data.add(new Data("F","017252525"));
+        data.add(new Data("G","0172523625"));
+        data.add(new Data("H","01725236525"));
+        data.add(new Data("I","01725222525"));
         data.add(new Data("Soyob","017252745"));
         data.add(new Data("Bisnu","017252525"));
         data.add(new Data("Badhon","0172523625"));
@@ -51,10 +52,38 @@ RecyclerView myRecycle;
         data.add(new Data("Noman","01725222525"));
         data.add(new Data("Soyob","017252745"));
 
+        adapter = new CustomAdapter(this,data);
         myRecycle = findViewById(R.id.recycleViewID);
         myRecycle.setLayoutManager(new LinearLayoutManager(this));
+        myRecycle.setAdapter(adapter);
 
-        myRecycle.setAdapter(new CustomAdapter(this,data));
+        FloatingActionButton flot_btn = findViewById(R.id.flot_btn);
+
+
+        flot_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialog);
+                dialog.show();
+
+                EditText number = dialog.findViewById(R.id.edt_number);
+                EditText name = dialog.findViewById(R.id.edt_name);
+
+            Button btn = dialog.findViewById(R.id.insert_btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    data.add(new Data(name.getText().toString(),number.getText().toString()));
+                    Toast.makeText(getApplicationContext(), "Insert Successfully", Toast.LENGTH_SHORT).show();
+                    adapter.notifyItemInserted(data.size()-1);
+                    myRecycle.scrollToPosition(data.size()-1);
+                    dialog.dismiss();
+                }
+            });
+
+            }
+        });
 
     }
 
